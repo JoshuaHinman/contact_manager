c15f8e7e4dd81071c4d8ac2e98d52b8206f17b15
Joshua Hinman <joshuahinman@gmail.com>
2022-05-04 13:38:05 -0700

    heroku config

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/', (req,res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
-app.listen(3000, () => console.log('Listening'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening'));
